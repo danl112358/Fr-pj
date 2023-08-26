18ae2580effdec9f8e12918cf32af352ca193c17
Daniel Bautista <bnj8tyuj@gmail.com>
2023-08-26 18:58:56 -0400

    responsive

--- switch.js
+++ switch.js
@@ -3,3 +3,13 @@ btnSwitch.addEventListener('click', ()=>{
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 })
+
+/*funcion del menu*/
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if (x.className === ""){
+        x.className = "responsive";
+    } else{
+        x.className = "";
+    }
+}
